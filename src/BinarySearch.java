a53a94af5461d2e280785409b76322ffac4d2e4f
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-02-15 09:59:46 +0530

    Binary Search

---
+++ src/BinarySearch.java
@@ -0,0 +1,28 @@
+public class BinarySearch {
+    public static int Search(int[] arr, int x) {
+        int s = 0;
+        int e = arr.length - 1;
+
+        while (s <= e) {
+          int   mid = s+(e-s) / 2;
+            if (arr[mid] > x) {
+                e = mid - 1;
+            } else if (arr[mid] < x) {
+                s = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 8, 14,};
+        int ch = 5;
+        int gh = Search(arr, ch);
+        System.out.println(gh);
+
+    }
+}
